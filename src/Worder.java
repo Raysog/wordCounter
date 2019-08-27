1b0296d55f0f1f61fc3e5a176bc5a486a1045c38
Федоров Иван < @10.227.45.37>
2019-08-27 13:16:33 +0300

    fixed comments

--- src/Worder.java
+++ src/Worder.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Map;
 import java.util.Scanner;
 import java.util.TreeMap;
@@ -13,24 +14,29 @@ public class Worder {
     private Map<String, Integer> statistics;
 
     public Worder() {
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Enter absolute or relative file path:");
-        String filePath = scan.nextLine();
-        File testFile = new File(filePath);
-        if (testFile.exists()) {
-            System.out.println("File downloaded");
-            this.file = testFile;
-        } else {
-            System.out.println("The specified file does not exist");
-            this.file = null;
-        }
+    	boolean correctPathFlag = false;
+    	Scanner scan = new Scanner(System.in);
+    	while (!correctPathFlag) {
+	        System.out.println("Enter absolute or relative file path:");
+	        String filePath = scan.nextLine();
+	        File testFile = new File(filePath);
+	        if (testFile.exists()) {
+	            System.out.println("File downloaded");
+	            this.file = testFile;
+	            correctPathFlag = true;
+	            scan.close();
+	        } else {
+	            System.out.println("The specified file does not exist");
+	            this.file = null;
+	        }
+    	}
+        
     }
 
     public void readFile() throws IOException {
-        if(file != null) {
             Scanner scanner = new Scanner(new File(file.getPath()));
             statistics = new TreeMap<>();
-            scanner.useDelimiter("[^a-zA-Z–∞-—è–ê-–Ø]+");
+            scanner.useDelimiter("[^a-zA-Z‡-ˇ¿-ﬂ]+");
             while (scanner.hasNext()) {
                 String word = scanner.next();
                 Integer count = statistics.get(word);
@@ -39,67 +45,46 @@ public class Worder {
                 }
                 statistics.put(word, ++count);
             }
-        }
+            scanner.close();
     }
 
     public void printMap(){
-        if (file != null) {
-            if (statistics.isEmpty()) {
-                System.out.println("File is empty");
-            } else {
-                System.out.println("All words: ");
-                for (String name : statistics.keySet()) {
-                    Integer value = statistics.get(name);
-                    System.out.println("key: " + name + " count: " + value + ";");
-                }
+    	if (statistics.isEmpty()){
+            System.out.println("File is empty");
+        } else {
+            for (String name : statistics.keySet()) {
+                Integer value = statistics.get(name);
+                System.out.println("key: " + name + " count: " + value + ";");
             }
         }
     }
+ 
 
-    private void printMap(Map<String, Integer> printing){
-        if(file != null)  {
+    public void findMaxWords(){
+    	ArrayList<String> words = null;
+        
             if (statistics.isEmpty()){
                 System.out.println("File is empty");
             } else {
-                for (String name : printing.keySet()) {
-                    Integer value = printing.get(name);
-                    System.out.println("key: " + name + " count: " + value + ";");
-                }
-            }
-        }
-    }
-
-    private void removeAll(Map<String, Integer> removing){
-        for (String key : statistics.keySet()) {
-            removing.remove(key);
-        }
-    }
-
-    public Map<String, Integer> findMaxWords(){
-        Map<String, Integer> max = null;
-        Integer maxCount = -1;
-        if(file != null) {
-            if (statistics.isEmpty()){
-                System.out.println("File is empty");
-            } else {
-                max = new TreeMap<>();
+            	Integer maxCount = -1;
+            	words = new ArrayList<>();
                 for (String key : statistics.keySet()) {
                     if (statistics.get(key) > maxCount) {
-                        if (!max.isEmpty()) {
-                            removeAll(max);
+                        if (!words.isEmpty()) {
+                        	words.removeAll(words);
                         }
                         maxCount = statistics.get(key);
-                        max.put(key, statistics.get(key));
+                        words.add(key);
                     } else if (statistics.get(key) == maxCount) {
                         maxCount = statistics.get(key);
-                        max.put(key, statistics.get(key));
+                        words.add(key);
                     }
                 }
                 System.out.println("Words occurring most times:");
-                printMap(max);
+                for (String s : words) {
+					System.out.println("key: " + s +  " count: " + maxCount + ";");
+				}
             }
-        }
-        return max;
     }
 
 }
